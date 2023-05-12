c4d57c72d4b21051f54928f4e7244ba1e7cad417
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2023-05-12 19:35:40 +0600

    heading and form of contact us part done

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,8 +4,34 @@ import PageTopStyleBg from '../Utilities/PageTopStyleBg';
 const Contact = () => {
     return (
         <div className='container pt-110'>
-            <div className="contact-headin">
-                
+            <div className="contact">
+                <div className="contact-heading">
+                    <div className="heading-texts">
+                        <h2 className="title">Let’s Make it Happen Together</h2>
+                        <p className="desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+                    </div>
+                </div>
+
+                <div className="contact-form">
+                    <form>
+                        <div className="name-email">
+                            <div className="name">
+                                <input type="text" placeholder='Name' required />
+                                <div className="line-after"></div>
+                            </div>
+                            <div className="email">
+                                <input type="email" placeholder='Email' required />
+                                <div className="line-after"></div>
+                            </div>
+
+                        </div>
+                        <div className="message">
+                            <input type="text" placeholder='Message' required />
+                            <div className="line-after"></div>
+                        </div>
+                        <button type="submit" className="custom-button">Send</button>
+                    </form>
+                </div>
             </div>
             <PageTopStyleBg></PageTopStyleBg>
         </div>
